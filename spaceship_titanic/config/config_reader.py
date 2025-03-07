e165c6834ec62b155e648a90a59fe428d1001db9
Viktoriya Klyueva <vika_klyueva@inbox.ru>
2025-03-07 07:24:45 +0700

    Model class

---
+++ spaceship_titanic/config/config_reader.py
@@ -0,0 +1,20 @@
+import toml
+from pathlib import Path
+
+
+def load_hyperparameters_from_poetry():
+    # Путь к pyproject.toml
+    pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
+
+    # Чтение файла
+    with open(pyproject_path, "r") as f:
+        config = toml.load(f)
+
+    # Извлечение гиперпараметров
+    hyperparameters = config.get("tool", {}).get("model", {}).get("hyperparameters", {})
+    return hyperparameters
+
+
+if __name__ == "__main__":
+    hyperparams = load_hyperparameters_from_poetry()
+    print(hyperparams)
--- spaceship_titanic/model.py
+++ spaceship_titanic/model.py
@@ -1,11 +1,50 @@
+import logging
+import argparse
+from catboost import CatBoostClassifier
+import pandas as pd
+import joblib
+from spaceship_titanic.config import config_reader
+
+
 class MyClassifierModel:
-    """Класс для реализации ML модели."""
-    def __init__(self, param1, param2):
-        self.param1 = param1
-        self.param2 = param2
+    def __init__(self):
+        self.target = ['Transported']
+        self.hyperparameters = config_reader.load_hyperparameters_from_poetry()
+        self.model = CatBoostClassifier(**self.hyperparameters)
 
-    def train(self, data):
-        pass
+    def train(self, dataset_path):
+        logging.info(f"Loading dataset from {dataset_path}")
+        data = pd.read_csv(dataset_path)
+        X = data.drop(self.target, axis=1)
+        y = data[self.target]
 
-    def predict(self, data):
-        pass
+        logging.info("Training model...")
+        self.model.fit(X, y)
+
+        logging.info("Saving model to ./data/model/")
+        joblib.dump(self.model, './data/model/model.pkl')
+
+    def predict(self, dataset_path):
+        logging.info(f"Loading dataset from {dataset_path}")
+        data = pd.read_csv(dataset_path)
+
+        logging.info("Loading model from ./data/model/")
+        self.model = joblib.load('./data/model/model.pkl')
+
+        logging.info("Making predictions...")
+        predictions = self.model.predict(data)
+        pd.DataFrame(predictions, columns=['predictions']).to_csv('./data/results.csv', index=False)
+        logging.info("Predictions saved to ./data/results.csv")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['train', 'predict'])
+    parser.add_argument('--dataset', required=True)
+    args = parser.parse_args()
+
+    model = MyClassifierModel()
+    if args.command == 'train':
+        model.train(args.dataset)
+    elif args.command == 'predict':
+        model.predict(args.dataset)
